e1411d8a622c83f10502160208321a8515b3745a
Nobody4931 <ardenxie1@gmail.com>
2023-04-22 19:35:53 -0400

    test: create simple tui connect 4 game adapter for the minimax algorithm

--- main.go
+++ main.go
@@ -10,43 +10,26 @@ func main() {
 	// TEST CODE, REMOVE LATER
 	opts := NewOptions()
 	game := NewGame(&opts)
-
-	game.Place(1, PlayerTwo)
-	game.Place(1, PlayerTwo)
-	game.Place(1, PlayerTwo)
-	game.Place(1, PlayerOne)
-
-	game.Place(2, PlayerOne)
-	game.Place(2, PlayerOne)
-
-	game.Place(3, PlayerTwo)
-	game.Place(3, PlayerOne)
-
-	fmt.Println(game.Minimax(PlayerOne).Next.Move)
-
-	/* rand.Seed(time.Now().Unix())
-
 	turn := PlayerOne
-	for i := 0; i < 6; i++ {
-		move := rand.Intn(opts.Cols)
-		for !game.Place(move, turn) {
-			move = rand.Intn(opts.Cols)
-		}
-		turn ^= PlayerXor
-	}
-
-	for _, row := range game.Board {
-		fmt.Println(row)
-	}
-
-	for _, consecs := range game.getAllConsecutives() {
-		fmt.Println(consecs)
-	}
 
 	for !game.IsGameOver() {
-		prediction := game.Minimax(turn).Next.Move
-		game.Place(prediction, turn)
-		fmt.Printf("%d: %d\n", turn, prediction)
+		for _, row := range game.Board {
+			fmt.Println(row)
+		}
+
+		if optimalMove := game.Minimax(turn); optimalMove.Next != nil {
+			fmt.Printf("\nRecommended move: %d\n", game.Minimax(turn).Next.Move)
+		} else {
+			fmt.Printf("\nyeah you probably lost good luck loser\n")
+		}
+
+		var move int
+		fmt.Printf("Player %d's move: ", turn)
+		fmt.Scanf("%d\n", &move)
+
+		game.Place(move, turn)
 		turn ^= PlayerXor
-	} */
+
+		fmt.Println()
+	}
 }
