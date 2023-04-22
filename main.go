43171c0bfe3ad28af6e0587b672f6da47aa4c89d
Nobody4931 <ardenxie1@gmail.com>
2023-04-22 17:12:09 -0400

    test: create temporary test code for the minimax algorithm

--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "math/rand"
+	// "time"
+)
 
 func main() {
 	// TEST CODE, REMOVE LATER
 	opts := NewOptions()
 	game := NewGame(&opts)
 
-	game.Place(0, PlayerTwo)
-	game.Place(0, PlayerTwo)
-	game.Place(0, PlayerTwo)
-	game.Place(0, PlayerOne)
-
+	game.Place(1, PlayerTwo)
 	game.Place(1, PlayerTwo)
 	game.Place(1, PlayerTwo)
 	game.Place(1, PlayerOne)
 
-	game.Place(2, PlayerTwo)
+	game.Place(2, PlayerOne)
 	game.Place(2, PlayerOne)
 
+	game.Place(3, PlayerTwo)
 	game.Place(3, PlayerOne)
 
-	fmt.Println(game.IsGameOver())
-	fmt.Println(game.Winner() == PlayerOne)
+	fmt.Println(game.Minimax(PlayerOne).Next.Move)
+
+	/* rand.Seed(time.Now().Unix())
+
+	turn := PlayerOne
+	for i := 0; i < 6; i++ {
+		move := rand.Intn(opts.Cols)
+		for !game.Place(move, turn) {
+			move = rand.Intn(opts.Cols)
+		}
+		turn ^= PlayerXor
+	}
+
+	for _, row := range game.Board {
+		fmt.Println(row)
+	}
+
+	for !game.IsGameOver() {
+		prediction := game.Minimax(turn).Next.Move
+		game.Place(prediction, turn)
+		fmt.Printf("%d: %d\n", turn, prediction)
+		turn ^= PlayerXor
+	} */
 }
